241880ef7f1e3fb02d821970ffe8028b7c8de24f
shashank <shashank944533@gmail.com>
2022-12-19 13:04:15 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/shashank/Mongo-golang/controllers"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+
+}
+
+func getSession() *mgo.Session {
+
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
